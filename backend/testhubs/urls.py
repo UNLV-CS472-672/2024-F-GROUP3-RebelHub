c07e5d29e191aad7300be243c2697a359829024a
misherwa <misherwa@unlv.nevada.edu>
2024-10-14 23:46:13 -0700

    Made basic implemented of the comment creation feature in views and updated the URLs

--- backend/testhubs/urls.py
+++ backend/testhubs/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import DummyHubList
+from .views import DummyHubList, CreateComment
 
 urlpatterns = [
     path('api/dummyhubs/', DummyHubList.as_view(), name='dummy-hub-list'),
+    path('posts/<int:post_id>/comments/', CreateComment.as_view(), name='comment-create'),
+
 ]
--- backend/testhubs/views.py
+++ backend/testhubs/views.py
@@ -2,6 +2,13 @@ from django.shortcuts import render
 from rest_framework import generics
 from .models import DummyHub
 from .serializers import DummyHubSerializer
+from .models import Post 
+from .models import Comment
+from .serializers import CommentSerializer
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework import status
+
 
 # Create your views here.
 
@@ -9,3 +16,11 @@ from .serializers import DummyHubSerializer
 class DummyHubList(generics.ListAPIView):
     queryset = DummyHub.objects.all()
     serializer_class = DummyHubSerializer
+
+class CreateComment(APIView):
+    permission_classes = [IsAuthenticated]
+    def post(self, request, post_id):
+        try:
+            post = Post.objects.get(pk=post_id)
+        except Post.DoesNotExist:
+            return Response({'error': 'Post not found'}, status=status.HTTP_404_NOT_FOUND)
