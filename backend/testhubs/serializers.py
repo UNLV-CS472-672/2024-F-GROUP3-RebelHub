596165a3d626a25f0b1f5cc379a6ec6c5e22588a
misherwa <misherwa@unlv.nevada.edu>
2024-10-15 23:10:56 -0700

    Updates to models and serializers. Made comment and Post classes in both files. The model for both the Post and Comment classes is for the data structure. The serializers allows the models to be converted to JSON. This is for the API responses and to validate the input request

--- backend/testhubs/serializers.py
+++ backend/testhubs/serializers.py
@@ -1,7 +1,23 @@
 from rest_framework import serializers
 from .models import DummyHub
+from .models import Comment
+from .models import Post
 
 class DummyHubSerializer(serializers.ModelSerializer):
     class Meta:
         model = DummyHub
         fields = '__all__'
+
+#To serilize the comment        
+class CommentSerializer(serializers.ModelSerializer):
+    #Create Serilized data based on the Comment  fields
+    class Meta:
+        model = Comment
+        fields = ['id', 'post', 'author', 'message ', 'timestamp', 'upvotes', 'downvotes']
+
+#To serilize the post  
+class PostSerializer(serializers.ModelSerializer):
+    #Create Serilized data based on the Post fields
+    class Meta:
+        model = Post
+        fields = ['id', 'author', 'title', 'message', 'timestamp', 'hub']  
