f3d29364d400e82d25545f2a8c627415c1cedd47
misherwa <misherwa@unlv.nevada.edu>
2024-10-15 23:47:27 -0700

    Updated the urls.py file for making or getting data on posts or comments. Finished the CreatePost class in the views.py file so that posts can be created or recived and viewd.

--- backend/testhubs/urls.py
+++ backend/testhubs/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from .views import DummyHubList, CreateComment
+from .views import DummyHubList, CreateComment, CreatePost
 
 urlpatterns = [
     path('api/dummyhubs/', DummyHubList.as_view(), name='dummy-hub-list'),
-    path('posts/<int:post_id>/comments/', CreateComment.as_view(), name='comment-create'),
 
+    # The CreatePost class used for getting all the post and creating new post
+    path('posts/', CreatePost.as_view(), name='post-list'),  
+   
+    # The CreateComment class used to show all comments and make comments
+    path('posts/<int:post_id>/comments/', CreateComment.as_view(), name='comment-list-create'),  
 ]
--- backend/testhubs/views.py
+++ backend/testhubs/views.py
@@ -5,6 +5,7 @@ from .serializers import DummyHubSerializer
 from .models import Post 
 from .models import Comment
 from .serializers import CommentSerializer
+from .serializers import PostSerializer
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import status
@@ -57,4 +58,30 @@ class CreateComment(APIView):
         
         # Serialize the comment data and return to client
         serializer = CommentSerializer(comments, many=True)
+        return Response(serializer.data)  
+    
+# CreatePost handles any POST
+class CreatePost(APIView):
+    # Check to authenticated 
+    permission_classes = [IsAuthenticated]  
+
+    # POST method to create a new post
+    def post(self, request):
+        # The serializer with the incoming  data request
+        serializer = PostSerializer(data=request.data)
+        
+        # Check if data is valid
+        if serializer.is_valid():  
+            # Save the post from the user and return a status that the post was created
+            serializer.save(author=request.user)  
+            return Response(serializer.data, status=status.HTTP_201_CREATED)  
+        # If data is invalid then return ab error status
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)  
+
+    # The GET method to get the post
+    def get(self, request):
+        # Get posts from the database
+        posts = Post.objects.all()  
+        # Then serialize the list of posts and return the serialized data
+        serializer = PostSerializer(posts, many=True)  
         return Response(serializer.data)  
