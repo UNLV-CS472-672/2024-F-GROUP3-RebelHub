00db3735b3cd90413dc9f5dcfcf3168da4cdd508
Aarush Misherwan <misherwa@ad.unlv.edu>
2024-10-16 13:21:20 -0700

    Made a seperate folder for Comments. Moved all things related to comments  from the testhubs folder to the Comments folder.

--- backend/Posts/urls.py
+++ backend/Posts/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
-from .views import PostList, CreatPost
+from .views import PostList, CreatePost
 
 urlpatterns = [
 
-    # The CreatePost class used for getting all the post and creating new post
+    # The PostList class used for getting all the post 
     path('posts/', PostList.as_view(), name='post-list'),  
    
-   # The CreatePost class used for getting all the post and creating new post
-    path('posts/create/', CreatPost.as_view(), name='post-create'),  
+   # The CreatePost class used for creating a new post
+    path('posts/create/', CreatePost.as_view(), name='post-create'),  
 
     # The CreateComment class used to show all comments and make comments
     #path('posts/<int:post_id>/comments/', CreateComment.as_view(), name='comment-list-create'),  
