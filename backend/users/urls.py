2a7693fbf5d30ee89e86c072e4400900b3f92331
Leslie Atterbury <atterbur@unlv.nevada.edu>
2024-10-25 20:21:22 -0700

    Update urls.py

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -3,10 +3,10 @@ from django.urls import include, path
 from users.views import CreateUserView, UserDetailView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 urlpatterns = [
-    path('api/users/register/',CreateUserView.as_view(),name='register'),
-    path('api/users/token/',TokenObtainPairView.as_view(),name='get_token'),
-    path('api/users/token/refresh/',TokenRefreshView.as_view(),name='refresh'),
-    path('api/users/currentUser/', UserDetailView.as_view(), name='user-detail'),
+    path('users/register/',CreateUserView.as_view(),name='register'),
+    path('users/token/',TokenObtainPairView.as_view(),name='get_token'),
+    path('users/token/refresh/',TokenRefreshView.as_view(),name='refresh'),
+    path('users/currentUser/', UserDetailView.as_view(), name='user-detail'),
 
 
 
