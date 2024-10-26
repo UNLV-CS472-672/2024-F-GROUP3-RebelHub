1bef3541dfd05f0cc0f04951b3ed870dec2137ce
Leslie Atterbury <atterbur@unlv.nevada.edu>
2024-10-25 20:42:18 -0700

    Update urls.py

--- backend/Posts/urls.py
+++ backend/Posts/urls.py
@@ -4,7 +4,7 @@ from .views import PostList, CreatePost, LikePost, DislikePost, PostDetail, Post
 urlpatterns = [
 
     # The PostList class used for getting all the post (GET) 
-    path('posts/', PostList.as_view(), name='post-list'),
+    path('posts/', PostList.as_view(), name='post-list'),  
    
     # The CreatePost class used for creating a new post (POST)
     path('posts/create/', CreatePost.as_view(), name='post-create'),  
