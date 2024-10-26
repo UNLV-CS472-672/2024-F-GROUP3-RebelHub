9ecfd64c0f77cd6e58488e49056e712650da9207
misherwa <misherwa@unlv.nevada.edu>
2024-10-26 15:36:16 -0700

    Created the Comments folder to the backend. This is a new feature that allows for commenting on posts or to other comments. The basic files and layout has been made, and will need to be modified correctly for the feature to work.

---
+++ backend/Comments/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from .views import CommentList, CommentCreate
+
+urlpatterns = [
+
+    # The CommentList class used for getting all the post 
+    path('api/comments/', CommentList.as_view(), name='comment-list'),  
+   
+   # The CommentCreate class used for creating new post
+    path('api/comments/create/', CommentCreate.as_view(), name='comment-create'),  
+ 
+    # The CreateComment class used to show all comments and make comments
+    #path('posts/<int:post_id>/comments/', CreateComment.as_view(), name='comment-list-create'),  
+]
