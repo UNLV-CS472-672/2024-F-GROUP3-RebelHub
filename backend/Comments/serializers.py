5c365c43d5895338e549a3c0393adcb5a96caeff
misherwa <misherwa@unlv.nevada.edu>
2024-10-26 21:06:53 -0700

    Updated serializers.py to be used when serilized comment data. The fields are based on things like the id and post. Then there is data validation done through an update method. It checks for changes in likes, dislikes, and the message to update the comment.

--- backend/Comments/serializers.py
+++ backend/Comments/serializers.py
@@ -1,9 +1,27 @@
 from rest_framework import serializers
 from .models import Comment
 
-#To serilize the comment        
+# To serilize the comment        
 class CommentSerializer(serializers.ModelSerializer):
-    #Create Serilized data based on the Comment  fields
+    # Create Serilized data based on the Comment fields
     class Meta:
         model = Comment
-        fields = ['id', 'post', 'author', 'message ', 'timestamp', 'upvotes', 'downvotes']
+        fields = ['id', 'post', 'author', 'message ', 'timestamp', 'likes', 'dislikes']
+        read_only_fields = ['author', 'post'] 
+        
+
+        # When given a validated data, update and return the existing Comment instance.  
+        def update(self, instance, validated_data):
+            # Update likes, dislikes and the message
+            if 'likes' in validated_data:
+                instance.likes = validated_data.get('likes', instance.likes)
+
+            if 'dislikes' in validated_data:
+                instance.dislikes = validated_data.get('dislikes', instance.dislikes)
+    
+            if 'message' in validated_data:
+                instance.message = validated_data.get('message', instance.message)
+
+            # Save updated comment
+            instance.save()
+            return instance
