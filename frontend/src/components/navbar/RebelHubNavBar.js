f6954b03cffc903b2ea8d4b1c6cccf4d36d14581
Kevin Barrios <barrik3@unlv.nevada.edu>
2024-10-29 02:29:50 -0700

    added functions for button presses

--- frontend/src/components/navbar/RebelHubNavBar.js
+++ frontend/src/components/navbar/RebelHubNavBar.js
@@ -1,16 +1,31 @@
 "use client"
-import React from 'react';
+import React, { useState } from 'react';
+import { useRouter } from 'next/navigation';
 import styles from './RebelHubNavBar.module.css';
 
 
+const avatarIconPath = "/navbar/icons/avatar.png" // path for the avatar icon.
+const messageIconPath = "/navbar/icons/messageicon.png" // path for the message icon.
+const settingsIconPath = "/navbar/icons/settings.png" // path for the settings icon.
+const logoLightThemePath = "/navbar/logo/logo_light_alt.png" // path for the light theme logo.
+const logoDarkThemePath = "/navbar/logo/logo_dark_alt.png" // path for the dark theme logo.
+const logoRebelPath = "/navbar/logo/logo.png" // path for the rebel version logo.
 
 
+const AccountComponent = (props) => {
+	const [displayName, setDisplayName] = useState(props.username);
+	const [darkTheme] = useState(props.darkTheme);
+	const router = useRouter();
 
-const AccountComponent = () => {
+	const accountButtonPressed = () => {
+		console.log("pressed acc")
+		//router.push("/"); <--- go to the user's profile page.
+	};
+
 	return(
-		<button className={styles.accountButton}> 
-			<img src="avatar.png" className={styles.avatarIcon}/> 
-			<p className={styles.accountText}>Profile Name</p>
+		<button className={styles.accountButton} onClick={accountButtonPressed}> 
+			<img src={avatarIconPath} className={styles.avatarIcon}/> 
+			<p className={`${styles.accountText} ${darkTheme ? styles.dark : styles.light}`}>{displayName}</p>
 		</button>
 	);
 }
@@ -18,21 +33,41 @@ const AccountComponent = () => {
 
 const RebelHubNavBar = () => {
 
+	const [darkTheme, setDarkTheme] = useState(true);
+
+	const homeButtonPressed = () => {
+		console.log("pressed home button");
+		//router.push("/"); <--- go to the home page/timeline
+	};
+	const messageButtonPressed = () => {
+		console.log("pressed message button");
+		//router.push("/"); <--- go to the dms page 
+	};
+	const settingsButtonPressed = () => {
+		console.log("pressed settings button");
+		setDarkTheme(!darkTheme);
+		//router.push("/"); <--- go to the settigns page
+	};
+
 	return (
 		<>
-		<nav className={styles.nav}>
-			<button className={styles.homeButton}> <img src="rebelhub.png" className={styles.homeButtonLogo}/> </button>
+		<nav className={`${styles.nav} ${darkTheme ? styles.dark : styles.light}`}>
+			<button className={styles.homeButton} onClick={homeButtonPressed}>
+				<img src={darkTheme ? logoDarkThemePath : logoLightThemePath} className={styles.homeButtonLogo}/>
+			</button>
 			<input className={styles.searchBar} type="text" placeholder="Search..." />
-			<button className={styles.messageButton}> <img src="messageicon.png" className={styles.messageIcon}/>  </button>
-			<button className={styles.settingsButton}> <img src="settings.png" className={styles.settingsIcon}/> </button>
-			<AccountComponent/>
+			<button className={styles.messageButton} onClick={messageButtonPressed}>
+				<img src={messageIconPath} className={styles.messageIcon}/>
+			</button>
+			<button className={styles.settingsButton} onClick={settingsButtonPressed}>
+				<img src={settingsIconPath} className={styles.settingsIcon}/>
+			</button>
+			<AccountComponent username="some user" darkTheme={darkTheme}/>
 		</nav>
+		<div className={styles.extraSpace}> </div>
 		</>
 	);
 };
 
 
 export default RebelHubNavBar;
-
-
-//<button className={styles.accountButton}> Account </button>
