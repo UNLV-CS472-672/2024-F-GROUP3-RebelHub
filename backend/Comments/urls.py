ea7cb1cad47fe1a5f189267146f72db3d2b2bb52
misherwa <misherwa@unlv.nevada.edu>
2024-10-29 22:13:08 -0700

    Updated urls.py to include the comment list and create. Also made endpoints for liking or disliking a comment.

--- backend/Comments/urls.py
+++ backend/Comments/urls.py
@@ -1,14 +1,12 @@
 from django.urls import path
-from .views import CommentList, CommentCreate
+from .views import CommentList, CommentCreate, LikeComment, DislikeComment
 
 urlpatterns = [
-
-    # The CommentList class used for getting all the post 
-    path('api/comments/', CommentList.as_view(), name='comment-list'),  
-   
-   # The CommentCreate class used for creating new post
-    path('api/comments/create/', CommentCreate.as_view(), name='comment-create'),  
- 
-    # The CreateComment class used to show all comments and make comments
-    #path('posts/<int:post_id>/comments/', CreateComment.as_view(), name='comment-list-create'),  
+    # Used to get the all comments (GET), and create comments (POST)
+    path('comments/', CommentList.as_view(), name='comment-list'),
+    path('comments/create/', CommentCreate.as_view(), name='comment-create'),
+    
+    # Endpoints for liking and disliking a comment using buttons from the  frontend
+    path('comments/<int:comment_id>/like/', LikeComment.as_view(), name='like-comment'),
+    path('comments/<int:comment_id>/dislike/', DislikeComment.as_view(), name='dislike-comment'),
 ]
