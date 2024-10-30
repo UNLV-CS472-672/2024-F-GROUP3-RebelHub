9bcacc7c1fd9644a644d783e4e5301401488a16c
misherwa <misherwa@unlv.nevada.edu>
2024-10-29 22:13:33 -0700

    Updated urls.py to fix a comment issue.

--- backend/Comments/urls.py
+++ backend/Comments/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('comments/', CommentList.as_view(), name='comment-list'),
     path('comments/create/', CommentCreate.as_view(), name='comment-create'),
     
-    # Endpoints for liking and disliking a comment using buttons from the  frontend
+    # Endpoints for liking and disliking a comment using buttons from the frontend
     path('comments/<int:comment_id>/like/', LikeComment.as_view(), name='like-comment'),
     path('comments/<int:comment_id>/dislike/', DislikeComment.as_view(), name='dislike-comment'),
 ]
