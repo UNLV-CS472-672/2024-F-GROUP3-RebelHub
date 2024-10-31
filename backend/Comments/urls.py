e8fd9bbe2b6f4f29d37c7d2ce4825828c379cad3
misherwa <misherwa@unlv.nevada.edu>
2024-10-31 00:32:40 -0700

    Updated the endpoints for urls.py to require posts for getting comments or creating them. Also added comments URL to rebel_hubs in the urls.py

--- backend/Comments/urls.py
+++ backend/Comments/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
-from .views import CommentList, CommentCreate, LikeComment, DislikeComment
+from .views import CommentList, CommentCreate, LikeComment, DislikeComment, CommentDetail
 
 urlpatterns = [
     # Used to get the all comments (GET), and create comments (POST)
-    path('comments/', CommentList.as_view(), name='comment-list'),
-    path('comments/create/', CommentCreate.as_view(), name='comment-create'),
+    path('posts/<int:post_id>/comments/', CommentList.as_view(), name='comment-list'),
+    path('posts/<int:post_id>/comments/create/', CommentCreate.as_view(), name='comment-create'),
     
+    # Used for getting, updating, or deleting a comment
+    path('comments/<int:comment_id>/', CommentDetail.as_view(), name='comment-detail'),
+
     # Endpoints for liking and disliking a comment using buttons from the frontend
     path('comments/<int:comment_id>/like/', LikeComment.as_view(), name='like-comment'),
     path('comments/<int:comment_id>/dislike/', DislikeComment.as_view(), name='dislike-comment'),
