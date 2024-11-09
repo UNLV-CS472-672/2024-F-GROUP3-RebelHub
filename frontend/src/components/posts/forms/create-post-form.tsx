82fdc89c4ca558016a725144f6021cb6dfcc0a4b
Leslie Atterbury <atterbur@unlv.nevada.edu>
2024-11-08 16:25:35 -0800

    Update create-post-form.tsx
    Modified how the create post form inputs are handled to allow greater modularity.

--- frontend/src/components/posts/forms/create-post-form.tsx
+++ frontend/src/components/posts/forms/create-post-form.tsx
@@ -2,13 +2,13 @@
 
 import { FC } from "react";
 import { FormProvider, useForm } from "react-hook-form";
-import CreatePostInput from "@/components/posts/forms/create-post-input";
+import CreateInput from "@/components/posts/forms/create-input";
 import { TITLE_VALIDATION, POST_MESSAGE_VALIDATION } from "@/utils/posts/create-post-validations";
 import { getCreatePostURL, URL_SEGMENTS } from "@/utils/posts/url-segments";
-import styles from "../posts.module.css";
+import styles from "./create-post-form.module.css";
 import api from "@/utils/api";
 import { useRouter } from "next/navigation";
-import HubInput from "./hub-input";
+import HubInput from "./create-input-hub";
 
 const CreatePostForm: FC = () => {
     const methods = useForm();
@@ -59,9 +59,9 @@ const CreatePostForm: FC = () => {
                     <h1>Create A Post</h1>
                 </div>
                 <div className={styles.createPostContainer}>
-                    <CreatePostInput {...TITLE_VALIDATION} />
+                    <CreateInput {...TITLE_VALIDATION} />
                     <HubInput/>
-                    <CreatePostInput {...POST_MESSAGE_VALIDATION} />
+                    <CreateInput {...POST_MESSAGE_VALIDATION} />
                 
                     <div className={styles.createPostButtonContainer}>
                         <button className={styles.createPostButton} onClick={onSubmit}>
