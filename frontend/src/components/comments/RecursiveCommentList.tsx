7ec3648965a07465212f710bb4fdaa14ecbe909b
Leslie Atterbury <atterbur@unlv.nevada.edu>
2024-11-10 00:02:31 -0800

    Create RecursiveCommentList.tsx
    Takes a post and calls a recursive comment component for each comment that is directly on the post.

---
+++ frontend/src/components/comments/RecursiveCommentList.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+import api from "@/utils/api";
+import { Post, PostComment } from "@/utils/posts/definitions";
+import { getCommentsListUrl } from "@/utils/url-segments";
+import { useEffect, useState } from "react";
+import RecursiveComment from "./RecursiveComment";
+import styles from "./RecursiveCommentList.module.css";
+
+interface ComponentProps {
+    post: Post;
+    commentsToPrint?: number;
+}
+
+const RecursiveCommentList: React.FC<ComponentProps> = ({ post, commentsToPrint=null }) => {
+    const [comments, setComments] = useState<PostComment[]>([]);
+
+    useEffect(() => {
+        const fetchComments = async () => {
+            try {
+                const response = await api.get(getCommentsListUrl(post.id));
+
+                if(response.status == 200) {
+                    console.log("got comment list");
+
+                    // Decide whether to print all base comments or just a specific amount
+                    if(commentsToPrint == null || commentsToPrint >= response.data.length) {
+                        setComments(response.data);
+                    } else {
+                        setComments(response.data.slice(0, commentsToPrint));
+                    }
+                    
+                }
+
+            } catch (error) {
+                alert(error);
+            }    
+        }
+
+        fetchComments();
+    }, []);
+
+    return (
+        <div>
+            {
+                comments.map((comment) => (
+                    <div className={styles.listContainer} key={comment.id}>
+                        <RecursiveComment currentComment={comment} />
+                    </div>
+                ))
+            }
+        </div>
+    );
+}
+
+export default RecursiveCommentList;
