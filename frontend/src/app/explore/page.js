3a8a03df77fb4bccefadb7d9c8da07b25868bd5a
Nathan Nguyen <nnguyen21697@gmail.com>
2024-11-13 02:07:51 -0800

    Adjusted explore's page.js to include the sidebar and navbar. Made sidebar width into vw in order to adjust for different browser sizes.

--- frontend/src/app/explore/page.js
+++ frontend/src/app/explore/page.js
@@ -3,13 +3,20 @@ import axios from 'axios';
 import { useEffect, useState } from 'react';
 import Explore from "../../components/Explore/Explore";
 import ProtectedRoute from '../../components/Accounts/ProtectedRoutes';
+import styles from "../../components/Explore/Explore.module.css"
+import SideBar from "../../components/sidebar.js"
+import RebelHubNavBar from "../../components/navbar/RebelHubNavBar.js"
 
 const Home = () => {
 	return (
 		<ProtectedRoute>
-			<main style={{ backgroundColor: '#f2f2f2'}}>
-				<Explore/>
-			</main>
+			<div className={styles.container}>
+				<RebelHubNavBar/>
+            	<SideBar />
+				<main style={{ backgroundColor: '#0a0a0a'}}>
+					<Explore/>
+				</main>
+			</div>
 		</ProtectedRoute>
 	);
 };
