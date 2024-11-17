79b334c3277638e8b299054c4dc078072e423cd0
misherwa <misherwa@unlv.nevada.edu>
2024-11-16 21:17:53 -0800

    Added things to import for test.py file.

--- backend/Private_Messaging/tests.py
+++ backend/Private_Messaging/tests.py
@@ -1,3 +1,5 @@
 from django.test import TestCase
-
+from rest_framework.test import APIClient
+from django.contrib.auth.models import User
+from .models import Conversation
 # Create your tests here.
