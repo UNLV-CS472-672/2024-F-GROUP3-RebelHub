dc78f2ea6f0454dd39d746631b4bee39d148b29f
misherwa <misherwa@unlv.nevada.edu>
2024-11-22 19:00:56 -0800

    Finished the models.py file. I added fetch messages to fetch all messages inside a conversation. I also made a seperate class for Message. Messages are added and fetched inside of the conversation class

--- backend/Private_Messaging/models.py
+++ backend/Private_Messaging/models.py
@@ -2,21 +2,36 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Conversation(models.Model):
-    conversationID = models.AutoField(primary_key=True)  # This is the only auto-generated field
+    
+    # This is the only auto-generated field
+    conversationID = models.AutoField(primary_key=True)  
     creatorID = models.ForeignKey(User, on_delete=models.CASCADE, related_name='created_conversations')
     participants = models.ManyToManyField(User, related_name='conversations')
     timestamp = models.DateTimeField(auto_now_add=True)
 
-    # Message-related fields (without auto-generated ID)
+
+    def __str__(self):
+        return str(self.conversationID)
+    
+    def add_message(self, user, content):
+        message = Message.objects.create(conversationID=self,userID=user,messageContent=content)
+        return message
+        
+    # Viewing all messages in conversation   
+    def fetch_messages(self):
+        messages = Message.objects.filter(conversationID=self).order_by('messageTimestamp')
+        return [{ "Message ID": message.messageID, "user ID": message.userID.id, "User Name": message.userID.username, 
+                 "Message Content": message.messageContent,"Message Timestamp": message.messageTimestamp,}
+            for message in messages
+        ]
+
+class Message(models.Model):
+    # Unique ID for each message
+    messageID = models.AutoField(primary_key=True)
+    conversationID = models.ForeignKey(Conversation, on_delete=models.CASCADE, related_name='messages')
     userID = models.ForeignKey(User, on_delete=models.CASCADE, related_name='message_user')
     messageContent = models.TextField()
     messageTimestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"Conversation {self.conversationID}"
-
-    def add_message(self, user, content):
-        self.userID = user
-        self.messageContent = content
-        self.messageTimestamp = models.DateTimeField(auto_now=True)
-        self.save()
+        return str(self.messageID)
