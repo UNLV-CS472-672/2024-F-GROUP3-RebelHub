738a85a5243a1dd109e12ddc3ff2f8a01621db97
misherwa <misherwa@unlv.nevada.edu>
2024-11-22 19:30:02 -0800

    Finished updateing the serializers.py file. I added a message serializer class based on what I had updated in the models. I also had to switch up the fields in the conversation serializer since I added the message class in models.

--- backend/Private_Messaging/serializers.py
+++ backend/Private_Messaging/serializers.py
@@ -1,9 +1,17 @@
 from rest_framework import serializers
 from .models import Conversation
+from .models import Message
 
+
+class MessageSerializer(serializers.ModelSerializer):
+    UserName = serializers.CharField(source="userID.username", read_only=True)
+    class Meta:
+        model = Message
+        fields = ["messageID","userID", "UserName", "messageContent","messageTimestamp",]
+        
 class ConversationSerializer(serializers.ModelSerializer):
+    messages = MessageSerializer(many=True, read_only=True)
+    participants = serializers.StringRelatedField(many=True)
     class Meta:
         model = Conversation
-        fields = [
-            'conversationID', 'creatorID', 'participants','timestamp', 'messageID', 'userID', 'messageContent', 'messageTimestamp'
-        ]
+        fields = ["conversationID","creatorID", "participants","timestamp","messages",]
