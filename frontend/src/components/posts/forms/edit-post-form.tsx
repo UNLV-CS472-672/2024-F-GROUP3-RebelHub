36403d154cf867ffdad8789f423f30c0f4df047a
Leslie Atterbury <atterbur@unlv.nevada.edu>
2024-11-23 23:44:06 -0800

    Update edit-post-form.tsx
    Modified the edit post form to use a FormData object to pass the inputted information to the backend. This allows an image to be sent to the backend.

--- frontend/src/components/posts/forms/edit-post-form.tsx
+++ frontend/src/components/posts/forms/edit-post-form.tsx
@@ -7,6 +7,9 @@ import { getEditPostUrl } from "@/utils/url-segments";
 import styles from "./edit-post-form.module.css";
 import api from "@/utils/api";
 import { Post } from "@/utils/posts/definitions";
+import { register } from "module";
+import ImageInput from "./ImageInput";
+import { headers } from "next/headers";
 
 interface ComponentProps {
     post: Post;
@@ -15,21 +18,42 @@ interface ComponentProps {
 }
 
 const EditPostForm: React.FC<ComponentProps> = ({ post, onClose, refreshComponent }) => {
-    const methods = useForm();
+    const methods = useForm({
+        defaultValues: {
+            title: post.title,
+            message: post.message,
+            image: post.image,
+        }
+    });
 
     const onSubmit = methods.handleSubmit(async data => {
         try {
+            console.log("Editing a post");
 
             /*
                 Additional error checking beyond the basic validations can go here.
             */
 
-            console.log("Editing a post");
+            /*
+                Now make the form data object to send to the backend.
+            */
 
-            const response = await api.patch(getEditPostUrl(post.id), {
-                title: data["title"],
-                message: data["message"],
-                last_edited: new Date(),
+            let formData = new FormData();
+            
+            // If the image was changed in the form, then add the image to the 
+            // form data.
+            if (data["image"] != null && data["image"][0] instanceof File) {
+                formData.append("image", data["image"][0], data["image"][0].name);
+            }
+
+            formData.append("title", data["title"]);
+            formData.append("message", data["message"]);
+            formData.append("last_edited", new Date().toISOString());
+
+            const response = await api.patch(getEditPostUrl(post.id), formData, {
+                headers: {
+                    "Content-Type": "multipart/form-data",
+                },
             });
 
             if (response.status != 200) {
@@ -41,9 +65,10 @@ const EditPostForm: React.FC<ComponentProps> = ({ post, onClose, refreshComponen
             // We need to update the object in case the user tries to edit the post again.
             // If they try to edit the post again and we don't change the client object,
             // then the original title/message will be displayed in the form.
-            post.title = data["title"];
-            post.message = data["message"];
-            post.last_edited = new Date();
+            post.title = response.data.title;
+            post.message = response.data.message;
+            post.last_edited = response.data.last_edited;
+            post.image = response.data.image;
 
             // Instead of redirecting to a page, just use the function to refresh
             // the component of the post
@@ -69,7 +94,8 @@ const EditPostForm: React.FC<ComponentProps> = ({ post, onClose, refreshComponen
                 <div className={styles.editPostContainer}>
                     <CreateInput {...{...TITLE_VALIDATION, 'startingValue': post.title}} />
                     <CreateInput {...{...POST_MESSAGE_VALIDATION, 'startingValue': post.message}} />
-                
+                    <ImageInput />
+
                     <div className={styles.editPostButtonContainer}>
                         <button className={styles.editConfirm} onClick={onSubmit}>
                             Submit Changes
