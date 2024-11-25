41ac7bca6569c911034dd48be504e2208ff9666d
Kevin Barrios <barrik3@unlv.nevada.edu>
2024-11-25 13:55:24 -0800

    modularized hub page

--- frontend/src/components/hubs/HubPage.js
+++ frontend/src/components/hubs/HubPage.js
@@ -4,10 +4,12 @@ import React from 'react';
 import styles from './HubPage.module.css';
 import api from '@/utils/api';
 import PostList from '@/components/posts/post-list';
-import { getHubUrl, getCurrentUserUrl, getPostsHubUrl , getAcceptoinHubUrl ,
-	 getDeclineJoinHubUrl, getKickHubUrl, getUpdateHubUrl} from "@/utils/url-segments";
+import MemberList from '@/components/hubs/MemberList';
+import HubEdit from '@/components/hubs/HubEdit';
+import { getHubUrl, getCurrentUserUrl, getPostsHubUrl , getUpdateHubUrl} from "@/utils/url-segments";
 import { convertUtcStringToLocalString } from '@/utils/datetime-conversion';
 /*
+ * HUBDATA:
  * id
  * name
  * description
@@ -20,14 +22,21 @@ import { convertUtcStringToLocalString } from '@/utils/datetime-conversion';
 const HubPage = ({id}) => {
 
 	const [hubData, setHubData] = useState([]);
+	const [refreshCount, setRefreshCount] = useState(0);
+
 	const [membersData, setMembersData] = useState([]);
 	const [pendingMembersData, setPendingMembersData] = useState([]);
-	const [hubPosts, setHubPosts] = useState([]);
-	const [refreshCount, setRefreshCount] = useState(0);
-	const [isEditing, setIsEditing] = useState(false);
-	const [editName, setEditName] = useState("");
-	const [editDescription, setEditDescription] = useState("");
 
+	const [hubPosts, setHubPosts] = useState([]);
+
+	const [isEditing, setIsEditing] = useState(false);
+
+
+	/*
+	 * Calls the get hub by id so we can store the hub info.
+	 *
+	 * we also get the posts list for this hub.
+	 */
 	useEffect(() => {
 		const getHubInfo = async () => {
 			    try {
@@ -55,6 +64,11 @@ const HubPage = ({id}) => {
 		getPostsHub();
      	}, [id, refreshCount]);
 
+	/* When hub data is loaded/updated we make sure to get a fresh
+	 * list of members and pending members and store them in our state.
+	 *
+	 * these member lists are passed to the memberlist componenet.
+	 */
 	useEffect(() => {
 		const getMembersInfo = async () => {
 			if(hubData.members)
@@ -98,105 +112,22 @@ const HubPage = ({id}) => {
 		console.log(pendingMembersData);
      	}, [hubData]);
 
-	const memberClick = () => {
-		alert("REDIRECT TO USERS PAGE");
-	}
-	const pendingMemberAddButton = async (id) => {
-		const req = {"user_id": id};
-		try{
-			const response = await api.put(getAcceptoinHubUrl(hubData.id), req);
-			console.log("added member into hub");
-			setRefreshCount((count) => count += 1);
-		} catch (error) {
-			alert(error);
-			console.log(error);
-		}
-	}
 
-	const pendingMemberRemoveButton = async (id) => {
-		const req = {"user_id": id};
-		try{
-			const response = await api.put(getDeclineJoinHubUrl(hubData.id), req);
-			console.log("removed member from pending");
-			setRefreshCount((count) => count += 1);
-		} catch (error) {
-			alert(error);
-			console.log("error removing from pending", error);
-		}
-	}
-	const memberKickButton = async (id) => {
-		const req = {"user_id": id};
-		try{
-			const response = await api.put(getKickHubUrl(hubData.id), req);
-			console.log("kicked member from hub");
-			setRefreshCount((count) => count += 1);
-		} catch (error) {
-			console.log(error);
-			alert(error);
-		}
-	}
-	const MemberItem = ({data, pending, hasPermission}) => {
-		console.log("member id: ", data.id);
-		return (
-			<>
-			<button className={styles.memberItemButton} onClick={memberClick} >{data.username} </button>
-
-			{pending && 
-				<button className={styles.acceptPendingButton} onClick={() => pendingMemberAddButton(data.id)}> ADD </button>
-			}
-			{pending &&
-				<button className={styles.declinePendingButton} onClick={() => pendingMemberRemoveButton(data.id)}> REMOVE </button>
-			}
-
-			{!pending && 
-			 hasPermission && 
-				<button className={styles.acceptPendingButton} onClick={() => memberKickButton(data.id)}> KICK </button>
-			}
-			</>
-		);
-	};
-
+	/*isEditing state management
+	 *
+	 * we keep keep track of editing by the accept, cancel, and edit button
+	 * edit button lives on the hub page and is visible if you're owner.
+	 *
+	 * accept and cancel buttons live in the HubEdit component, but
+	 * we pass in the acceptEdit and cancelEdit functions. That way
+	 * we keep control of the isEditing state.
+	 */
 	const editButtonPress = () => {
-		setIsEditing(!isEditing);
+		setIsEditing(true);
 		console.log("is editing : ", isEditing);
 	};
-
-	const created_date = convertUtcStringToLocalString(hubData.created_at);
-	const hubOwner = hubData.owned;
-	const hubMod = hubData.modding;
-	const hubJoined = hubData.joined;
-	const hubPrivate = hubData.private_hub;
-
-	const handleNameInputChange = (e) => setEditName(e.target.value);
-	const handleDescriptionInputChange = (e) => setEditDescription(e.target.value);
-
-	const HubNameInput = () => {
-		return(
-			<input 
-				type="text"
-				className={styles.editHubName}
-				placeholder="New Title" 
-				value={editName}
-				onChange={handleNameInputChange}
-			/>
-		);
-	};
-
-	const HubDescriptionInput = () => {
-		return(
-			<textarea
-				className={styles.editHubDescription}
-				cols="90"
-				rows="15"
-				placeholder="New Description"
-				value={editDescription}
-				onChange={handleDescriptionInputChange}
-			/>
-		);
-	};
-
-	const acceptEdit = async () => {
-		const updateInfo = {"name": editName, "description": editDescription};
+	const acceptEdit = async (name, description) => {
+		const updateInfo = {"name": name, "description": description};
 		try{
 			const response = await api.put(getUpdateHubUrl(hubData.id), updateInfo);
 			setIsEditing(false);
@@ -211,93 +142,85 @@ const HubPage = ({id}) => {
 		setRefreshCount((count) => count+=1);
 	};
 
+	const handleSuccess = (data) => {
+		console.log("Success!", data);
+		setRefreshCount((count) => count+=1);
+	};
+
+	//general info.
+	const hubOwner = hubData.owned;
+	const hubMod = hubData.modding;
+	const hubJoined = hubData.joined;
+	const hubPrivate = hubData.private_hub;
+	const created_date = convertUtcStringToLocalString(hubData.created_at);
+
 	const HubPageMainContent = () => {
 		return(
-			<>
-			<div className={styles.parentContentContainer} >
-				<div className={styles.postTitleContainer} >
-					<h1 className={styles.postTitle} > Latest Posts </h1>
+			<div className={styles.parentContentContainer}>
+				<div className={styles.postTitleContainer}>
+					<h1 className={styles.postTitle}> Latest Posts </h1>
 				</div>
-				{/* main content container for the hub page */}
 				<div className={styles.hubPageContentContainer}>
 					<PostList className={styles.postsList} posts={hubPosts}/>
 					<div className={styles.membersListsContainer}>
-						{/* first members list shows all the members of a hub*/}
-						<div className={styles.membersList}>
-							<h2 className={styles.membersListHeader} > Members </h2>
-							<ul>
-							{membersData.map((member, index) => (
-								<li className={styles.memberLI} key={index} >
-									<MemberItem data={member} hasPermission={hubOwner || hubMod} pending={false}/>
-								</li>
-							))}
-							</ul>
-						</div>
-						{/* if the user is the hub owner a hub moderator they will see
-							the pending members list */}
+						<MemberList 
+							hubId={hubData.id}
+							memberList={membersData}
+							isPending={false}
+							hasPermission={(hubOwner || hubMod)}
+							onSuccess={(data) => handleSuccess(data)}
+						/>
 						{(hubOwner || hubMod) && 
-							<div className={styles.pendingMembersList}>
-								<h2 className={styles.pendingMembersListHeader} > Pending </h2>
-								{pendingMembersData.length == 0 && <p style={{color:'black'}}> No pending Members </p>}
-								<ul>
-								{pendingMembersData.map((member, index) => (
-									<li className={styles.memberLI} key={index} >
-										<MemberItem data={member} hasPermission={true} pending={true}/>
-									</li>
-								))}
-								</ul>
-							</div>
+							<MemberList 
+								hubId={hubData.id}
+								memberList={pendingMembersData}
+								isPending={true}
+								hasPermission={true} 
+							/>
 						}
 					</div>
 				</div>
-			</div> {/* end of content container*/}
-			</>
+			</div> 
 		);
 	};
 
 	return (
-		<>
 		<div className={styles.pageBG} >
-			<div className={styles.hubViewContainer}>
-				{!isEditing && (
-					<>
+			{isEditing ? ( 
+				<HubEdit
+					hubId={hubData.id}
+					oldName={hubData.name}
+					oldDescription={hubData.description}
+					onClickAccept={acceptEdit}
+					onClickDecline={cancelEdit}
+				/>
+				
+			):
+				<div className={styles.hubViewContainer}>
 					<div className={styles.hubViewHeading}>
 						<h1 className={styles.hubName}> {hubData.name} </h1><br/>
-							{hubOwner && <button className={styles.updateHubInfoBtn} onClick={() => editButtonPress()}> Edit </button> }
+						{hubOwner && <button className={styles.updateHubInfoBtn} onClick={() => editButtonPress()}> Edit </button> }
 					</div>
 					<p className={styles.hubDescription}>{hubData.description} </p>
 					<div className={styles.hubViewDetails}>
 						<p className={styles.hubOwner}> Owned By: {hubData.owner} </p>
 						<p className={styles.hubTimestamp}> Created At: {created_date.slice(0, created_date.length-6)} </p>
 					</div>
-					</>
-				)}
-				{isEditing && (
-					<>
-					<div className={styles.editViewContainer}>
-						<div className={styles.hubViewHeading}>
-							<HubNameInput />
-						</div>
-						<HubDescriptionInput />
-						<button className={styles.acceptEditBtn} onClick={() => acceptEdit()}> Accept </button>
-						<button className={styles.cancelEditBtn} onClick={() => cancelEdit()}> Cancel </button>
-					</div>
-					</>
-				)}
-			</div>
+				</div>
+			}
+					
 			{/* the hubs calander events component can go here */}
 
 			 {/*nothing is where the request buttons should
 		            appear and any other content a private hub
 			    should display*/}
-			{hubPrivate ? (
-				hubJoined ? <HubPageMainContent /> : <p> NOTHING </p>
+			{(hubPrivate && !hubJoined) ? (
+				<p> NOTHING </p>
 			) : (
 				<HubPageMainContent/>
 			)}
 			
 		</div>
-		</>
 	);
 };
 
