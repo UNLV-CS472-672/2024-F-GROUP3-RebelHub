46508c3f02e8781ca5311c207c678febebc12c7a
Kevin Barrios <barrik3@unlv.nevada.edu>
2024-11-26 03:27:44 -0800

    member lists includes a modding section

--- frontend/src/components/hubs/MemberList.js
+++ frontend/src/components/hubs/MemberList.js
@@ -1,30 +1,45 @@
-import { getAcceptoinHubUrl, getDeclineJoinHubUrl, getKickHubUrl } from "@/utils/url-segments";
+"use client";
+import { getAcceptoinHubUrl, getDeclineJoinHubUrl, getKickHubUrl, getModsAddHubUrl, getModsRemoveHubUrl} from "@/utils/url-segments";
 import styles from './MemberList.module.css';
 import api from '@/utils/api';
 
-const MemberItem = ({data, pending, hasPermission, onClickMember, onClickPendingAdd, onClickPendingRemove, onClickKick}) => {
-	console.log("member id: ", data.id);
+const MemberItem = ({data, ownerId, pending, modList, hasPermission, onClickMember, onClickPendingAdd, onClickPendingRemove, onClickKick, onClickModAdd, onClickModRemove}) => {
 	return (
-		<>
-		<button className={styles.memberItemButton} onClick={() => onClickMember(data.id)} >{data.username} </button>
-		{pending && 
-			<button className={styles.acceptPendingButton} onClick={() => onClickPendingAdd(data.id)}> ADD </button>
-		}
-		{pending &&
-			<button className={styles.declinePendingButton} onClick={() => onClickPendingRemove(data.id)}> REMOVE </button>
-		}
-		{!pending && 
-		 hasPermission && 
-			<button className={styles.acceptPendingButton} onClick={() => onClickKick(data.id)}> KICK </button>
-		}
-		</>
+		<div className={styles.memberItem}>
+			<div className={styles.memberName}>
+				<button className={styles.memberItemButton} onClick={() => onClickMember(data.id)} >{data.username} </button>
+			</div>
+			<div className={styles.memberActions}>
+				{pending && 
+					<button className={styles.acceptPendingButton} onClick={() => onClickPendingAdd(data.id)}> ADD </button>
+				}
+				{pending &&
+					<button className={styles.declinePendingButton} onClick={() => onClickPendingRemove(data.id)}> REMOVE </button>
+				}
+				{!pending && 
+				 hasPermission &&
+				 !modList &&
+				 (!(data.id == ownerId)) &&
+					<>
+					<button className={styles.memberKickButton} onClick={() => onClickKick(data.id)}> KICK </button>
+					<button className={styles.memberAddModButton} onClick={() => onClickModAdd(data.id)}> ADD MOD </button>
+					</>
+				}
+				{modList &&
+				 hasPermission &&
+					<>
+					<button className={styles.memberRemoveModButton} onClick={() => onClickModRemove(data.id)}> REMOVE </button>
+					</>
+				}
+			</div>
+		</div>
 	);
 };
 
-const MemberList = ({hubId, memberList, isPending, hasPermission, onSuccess}) => {
+const MemberList = ({hubId, hubOwnerId, memberList, isPending, isModList, hasPermission, onSuccess}) => {
 	const memberClick = (id) => {
 		alert("REDIRECT TO USERS PAGE");
-	}
+	};
 	const pendingMemberAddButton = async (id) => {
 		const req = {"user_id": id};
 		try{
@@ -35,7 +50,7 @@ const MemberList = ({hubId, memberList, isPending, hasPermission, onSuccess}) =>
 			alert(error);
 			console.log(error);
 		}
-	}
+	};
 
 	const pendingMemberRemoveButton = async (id) => {
 		const req = {"user_id": id};
@@ -47,7 +62,7 @@ const MemberList = ({hubId, memberList, isPending, hasPermission, onSuccess}) =>
 			alert(error);
 			console.log("error removing from pending", error);
 		}
-	}
+	};
 
 	const memberKickButton = async (id) => {
 		const req = {"user_id": id};
@@ -59,24 +74,54 @@ const MemberList = ({hubId, memberList, isPending, hasPermission, onSuccess}) =>
 			console.log(error);
 			alert(error);
 		}
-	}
+	};
 
+	const modAddButton = async (id) => {
+		const req = {"user_id": id};
+		try{
+			const response = await api.put(getModsAddHubUrl(hubId), req);
+			console.log("added member to mods");
+			onSuccess(response.data);
+		} catch(error) {
+			console.log(error);
+			alert(error);
+		}
+	};
+
+	const modRemoveButton = async (id) => {
+		const req = {"user_id": id};
+		try{
+			const response = await api.put(getModsRemoveHubUrl(hubId), req);
+			console.log("removed member from mods");
+			onSuccess(response.data);
+		} catch(error) {
+			console.log(error);
+			alert(error);
+		}
+	};
 	/* list shows all the members of a hub if pending is not set */
 	return (
 		<div className={styles.membersList}>
-			<h2 className={styles.membersListHeader} > {isPending ? 'Pending' : 'Members'} </h2>
+			<h2 className={styles.membersListHeader} > {isPending ? 'Pending' : (isModList ? 'Mods' : 'Members') } </h2>
 			{memberList.length == 0 && <p style={{color:'black'}}> Nothing... </p>}
-			<ul>
+			<ul className={styles.memberList}>
 			{memberList.map((member, index) => (
-				<li className={styles.memberLI} key={index} >
+				<li className={styles.memberLI} 
+				    style={{backgroundColor: (member.id == hubOwnerId) ? 'rgba(255, 255, 255, 0.7)' :
+						    					 'rgba(227, 24, 55, 1.0)'}}
+				    key={index} >
 					<MemberItem 
 						data={member} 
-						hasPermission={hasPermission} 
+						ownerId={hubOwnerId}
 						pending={isPending}
+						modList={isModList}
+						hasPermission={hasPermission} 
 						onClickMember={memberClick}
 						onClickPendingAdd={pendingMemberAddButton}
 						onClickPendingRemove={pendingMemberRemoveButton}
 						onClickKick={memberKickButton}
+						onClickModAdd={modAddButton}
+						onClickModRemove={modRemoveButton}
 					/>
 				</li>
 			))}
