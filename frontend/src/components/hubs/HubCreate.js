3ca97fcc5afce0a737f5fe8796d3bf267e138447
Kevin Barrios <barrik3@unlv.nevada.edu>
2024-11-25 16:18:50 -0800

    proper error messages

--- frontend/src/components/hubs/HubCreate.js
+++ frontend/src/components/hubs/HubCreate.js
@@ -21,6 +21,10 @@ const HubCreate = () => {
 	const [hubName, setHubName] = useState("");
 	const [hubDescription, setHubDescription] = useState("");
 	const [hubPrivate, setHubPrivate] = useState(false);
+	const [nameError, setNameError] = useState(false);
+	const [descriptionError, setDescriptionError] = useState(false);
+	const [nameErrorMessage, setNameErrorMessage] = useState("");
+	const [descriptionErrorMessage, setDescriptionErrorMessage] = useState("");
 	const router = useRouter();
 
 	useEffect(() => {
@@ -45,14 +49,31 @@ const HubCreate = () => {
 			description: hubDescription,
 			private_hub: hubPrivate
 		}
-
 		try {
 			const response = await api.post(getCreateHubsUrl(), hubCreateData);
 			const id = response.data.id;
 			router.push(`/hubs/${id}`);
 		} catch(error){
-			console.log("error creatting hub", error);
-			alert(error.message);
+			console.log("error creating hub", error);
+			console.log(error.request.response);
+			var e1 = false;
+			var e2 = false;
+			if(error.response.data.name)
+			{
+				setNameErrorMessage(error.response.data.name[0].toUpperCase());
+				setNameError(true);
+				e1 = true;
+			}
+			if(error.response.data.description)
+			{
+				setDescriptionErrorMessage(error.response.data.name[0].toUpperCase());
+				setDescriptionError(true);
+				e2 = true;
+			}
+			if(!e1 && !e2)
+			{
+				alert("Something went wrong! Try Again Later.\n" + error.message);
+			}
 		}
 	}
 
@@ -74,9 +95,11 @@ const HubCreate = () => {
 					type="text"
 					placeholder="Enter a name for your hub."
 					value={hubName}
-					onChange={(text) => setHubName(text.target.value)}
+					onChange={(text) => {setNameError(false);
+							     setNameErrorMessage("");
+						  	     setHubName(text.target.value);} }
 				/>
-
+				{nameError && <p> {nameErrorMessage} </p> }
 				<h2 className={styles.createDescHeader} > Hub Description </h2>
 				<textarea 
 					className={styles.createDescInput}
@@ -84,9 +107,11 @@ const HubCreate = () => {
 					rows="15"
 					placeholder="Enter a description for your hub."
 					value={hubDescription}
-					onChange={(text) => setHubDescription(text.target.value)}
+					onChange={(text) => {setDescriptionError(false); 
+							     setDescriptionErrorMessage(""); 
+							     setHubDescription(text.target.value);} }
 				/>
-
+				{descriptionError && <p> {descriptionErrorMessage} </p> }
 				<div className={styles.createRadioContainer} >
 					<input 
 						type="radio"
