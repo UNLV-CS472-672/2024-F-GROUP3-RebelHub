a4df5843737fd8bc503854d88f8c6d51d4570072
misherwa <misherwa@unlv.nevada.edu>
2024-11-27 22:17:04 -0800

    Invalid user test comes first. then the adding message test is second in the test.py file

--- backend/Private_Messaging/tests.py
+++ backend/Private_Messaging/tests.py
@@ -9,11 +9,11 @@ class ConversationModelTest(TestCase):
         self.conversation = Conversation.objects.create(creator_id=self.user1)
         self.conversation.participants.add(self.user1, self.user2)
 
+    def test_add_message_with_invalid_user(self):
+        with self.assertRaises(ValueError):
+            self.conversation.add_message(user=None, content="Chat Failed")
+            
     def test_add_message(self):
         message = self.conversation.add_message(user=self.user1, content="Hey it's me, wanna chat?")
         self.assertEqual(message.message_content, "Hey it's me, wanna chat?")
         self.assertEqual(message.user_id, self.user1)
-
-    def test_add_message_with_invalid_user(self):
-        with self.assertRaises(ValueError):
-            self.conversation.add_message(user=None, content="Chat Failed")
