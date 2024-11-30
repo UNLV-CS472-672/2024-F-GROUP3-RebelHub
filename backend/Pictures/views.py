f7b5fade627637932d1edbd2ab9b74c0f2b0bd3b
Leslie Atterbury <atterbur@unlv.nevada.edu>
2024-11-29 17:51:48 -0800

    Update views.py
    Added a edit post picture and delete post picture view.

--- backend/Pictures/views.py
+++ backend/Pictures/views.py
@@ -6,7 +6,7 @@ from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
 from .models import Picture, Post
 from rest_framework.response import Response
-from .serializers import CreatePostPictureSerializer, PictureSerializer
+from .serializers import CreatePostPictureSerializer, EditPostPictureSerializer, PictureSerializer
 from rest_framework import status, generics
 from rest_framework.parsers import MultiPartParser, FormParser
 from rest_framework.pagination import PageNumberPagination
@@ -16,12 +16,44 @@ class AddPictureToPostView(generics.CreateAPIView):
     queryset = Picture.objects.all()
     serializer_class = CreatePostPictureSerializer
     parser_classes = (MultiPartParser, FormParser)
+    permission_classes = [IsAuthenticated]
 
     def get_serializer_context(self):
         context = super().get_serializer_context()
         context['post_id'] = self.kwargs['post_id']
         return context
 
+class EditPictureInPostView(generics.UpdateAPIView):
+    queryset = Picture.objects.all()
+    serializer_class = EditPostPictureSerializer
+    parser_classes = (MultiPartParser, FormParser)
+    permission_classes = [IsAuthenticated]
+    lookup_field = "id"
+
+    def get_serializer_context(self):
+        context = super().get_serializer_context()
+        context['id'] = self.kwargs['id']
+        return context
+
+    def perform_update(self, serializer):
+        serializer.save()
+
+class DeletePictureInPostView(generics.DestroyAPIView):
+    queryset = Picture.objects.all()
+    permission_classes = [IsAuthenticated]
+    lookup_field = "id"
+
+    def perform_destroy(self, instance):
+        user = self.request.user
+
+        if user != instance.user:
+            post = instance.post
+
+            if user not in post.hub.mods.all():
+                if user != post.hub.owner:
+                    raise PermissionError()
+
+        instance.delete()
 
 class PicturePagination(PageNumberPagination):
     page_size = 9  
