5855020ef93bbd227ef424a9737bc0c6ec4e6afb
Leslie Atterbury <atterbur@unlv.nevada.edu>
2024-11-29 17:50:19 -0800

    Add edit picture serializer
    Added a serializer to help change an image for an existing picture object.

--- backend/Pictures/serializers.py
+++ backend/Pictures/serializers.py
@@ -15,9 +15,12 @@ class PictureSerializer(serializers.ModelSerializer):
         return obj.image.url if obj.image else None
 
 class CreatePostPictureSerializer(serializers.ModelSerializer):
+    image_url = serializers.SerializerMethodField()
+
     class Meta:
         model = Picture
-        fields = ['id', 'image']
+        fields = ['id', 'image', 'image_url']
+        read_only = ['id', 'image_url']
 
     def validate(self, data):
         request = self.context.get('request')
@@ -34,3 +37,33 @@ class CreatePostPictureSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         new_pic = Picture.objects.create(**validated_data)
         return new_pic
+
+    def get_image_url(self, obj):
+        return obj.image.url if obj.image else None
+
+class EditPostPictureSerializer(serializers.ModelSerializer):
+    image = serializers.ImageField()
+    image_url = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Picture
+        fields = ['image', 'image_url']
+        read_only = ['image_url']
+    
+    def validate(self, data):
+        pic_id = self.context.get('id')
+
+        get_object_or_404(Picture, pk=pic_id)
+        
+        return data
+
+    def update(self, instance, validated_data):
+        image = validated_data.get('image')
+
+        instance.image = image
+        instance.save()
+        
+        return instance
+
+    def get_image_url(self, obj):
+        return obj.image.url if obj.image else None
