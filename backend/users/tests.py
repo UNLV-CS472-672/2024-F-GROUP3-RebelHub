3cf52516cb718087b4b9c7b365e501ac6509de16
misherwa <misherwa@unlv.nevada.edu>
2024-12-03 11:50:38 -0800

    Made 100 percent test coverage in user test.py file.

--- backend/users/tests.py
+++ backend/users/tests.py
@@ -3,7 +3,32 @@ from rest_framework.test import APIRequestFactory, force_authenticate
 from rest_framework import status
 from users.views import CreateUserView, UserDetailView
 from django.contrib.auth.models import User
+import json
+from django.urls import reverse
+from rest_framework.test import APITestCase
 
+class CustomPasswordResetViewTestCase(APITestCase):
+    def setUp(self):
+        self.password_reset_url = reverse('password_reset')
+        self.user = User.objects.create_user(username="testuser",email="testuser@example.com",password="password")
+
+    def test_password_reset_success(self):
+        response = self.client.post(self.password_reset_url, {"email": self.user.email})
+        response_data = json.loads(response.content)
+        self.assertEqual(response.status_code, 200)
+        self.assertIn("message", response_data)
+
+    def test_password_reset_invalid_email(self):
+        response = self.client.post(self.password_reset_url, {"email": "invalid@example.com"})
+        response_data = json.loads(response.content)
+        self.assertEqual(response.status_code, 200)  
+        self.assertIn("message", response_data)
+
+    def test_password_reset_missing_email(self):
+        response = self.client.post(self.password_reset_url, {})
+        response_data = json.loads(response.content)
+        self.assertEqual(response.status_code, 400)
+        self.assertIn("error", response_data)
 
 # Create your tests here.
 class UserTestCase(TestCase):
