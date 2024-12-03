9b112273274593414b0f3effcae6563dae9bec9e
misherwa <misherwa@unlv.nevada.edu>
2024-12-03 14:02:36 -0800

    Remove testhubs folder

--- backend/testhubs/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class TesthubsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'testhubs'
--- backend/testhubs/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import DummyHub
-
-class DummyHubSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = DummyHub
-        fields = '__all__'
